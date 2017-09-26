0647d5e344b3c90380da9f245a1eb29831f3b743
campusboy87 <dmh87@mail.ru>
2017-09-26 22:58:21 +0300

    Изменен метод получение данных для тега guid

--- templates/feed.php
+++ templates/feed.php
@@ -25,7 +25,7 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
 			<item>
 				<title><?php the_title_rss(); ?></title>
 				<link><?php the_permalink_rss(); ?></link>
-				<guid><?php echo esc_html( wp_get_shortlink() ); ?></guid>
+				<guid><?php the_guid(); ?></guid>
 				<pubDate><?php echo get_post_time( 'r', true ); ?></pubDate>
 				<author><?php the_author(); ?></author>
 				<description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
